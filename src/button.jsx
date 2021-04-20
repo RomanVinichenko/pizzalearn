d931f80379e9036ef0f3375dd11a443d0031a7e8
roman <elik@example.com>
2021-04-20 23:46:02 +0300

    2.1

---
+++ src/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="button button--outline">{this.props.text}</button>
+    );
+  }
+}
+
+export default Button;
